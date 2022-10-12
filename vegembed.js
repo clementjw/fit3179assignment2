1544bfa642d7ec80e697a1f88f16944d88e55706
Jia Wen <64416000+clementjw@users.noreply.github.com>
2022-10-12 23:22:54 +0800

    Add files via upload

---
+++ vegembed.js
@@ -0,0 +1,6 @@
+var vg_1 = "genderrankmap.json";
+var vg_2 = "linegraph.json";
+var vg_3 = "splom.json"
+vegaEmbed("#regionMap", vg_1).then(function(result) {}).catch(console.error);
+vegaEmbed("#linegraph", vg_2).then(function(result) {}).catch(console.error);
+vegaEmbed("#splom", vg_3).then(function(result) {}).catch(console.error);
