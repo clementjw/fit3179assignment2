912d39e3f1c5799a07639480d98b6266d80ec456
Jia Wen <64416000+clementjw@users.noreply.github.com>
2022-10-15 03:14:39 +0800

    Add files via upload

--- vegembed.js
+++ vegembed.js
@@ -2,7 +2,11 @@ var vg_1 = "genderrankmap.json";
 var vg_2 = "linegraph.json";
 var vg_3 = "splom.json";
 var vg_4 = "heatmap.json"
+var vg_5 = "bumpchart.json"
 vegaEmbed("#regionMap", vg_1).then(function(result) {}).catch(console.error);
 vegaEmbed("#linegraph", vg_2).then(function(result) {}).catch(console.error);
 vegaEmbed("#splom", vg_3).then(function(result) {}).catch(console.error);
 vegaEmbed("#Heatmap", vg_4).then(function(result) {}).catch(console.error);
+vegaEmbed("#bumpChart", vg_5).then(function(result) {}).catch(console.error);
+
+
